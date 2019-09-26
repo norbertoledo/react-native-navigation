44536fa2083d332688c64053aa7153f588889805
norbertoledo <norbertoledo@gmail.com>
2019-09-26 02:03:17 +0200

    Clientes openDrawer

--- components/ClientesScreen.js
+++ components/ClientesScreen.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { View, Text, StyleSheet, Button, TouchableHighlight } from 'react-native'
 
 const ClientesScreen = ({navigation}) => {
+    
+    // const parent = navigation.dangerouslyGetParent();
+    // const isDrawerOpen = parent && parent.state && parent.state.isDrawerOpen;
+
+    useEffect(
+        ()=>{
+
+            // if(!isDrawerOpen) 
+            navigation.openDrawer();
+        },[]
+    )
+    // 
+    
     return (
         <View style={styles.container}>
             
